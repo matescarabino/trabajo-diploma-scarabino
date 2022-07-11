08df441279d15944bf2aba8ac99553f9d0440331
Mateo Scarabino <91098568+matescarabino@users.noreply.github.com>
2022-07-10 23:44:07 -0300

    prueba bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      
+      <NavBar />
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          buenas <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
